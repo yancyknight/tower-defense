ef1390329b5af6b274947d6b6fbd3f56945219e3
Yancy Knight <yancy.knight@gmail.com>
2018-04-18 20:16:38 -0600

    Fix horizontal flip.

--- framework/graphics.js
+++ framework/graphics.js
@@ -114,7 +114,7 @@ function drawImage({
 		context.translate(-center.x, -center.y);
 		if (horizontalFlip) {
 			context.scale(-1, 1);
-			dx = -dx;
+			dx = -dx - dWidth;
 		}
 
 		context.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
@@ -181,7 +181,7 @@ function SpriteSheet({
 			sy: 0,
 			sWidth: width,
 			sHeight: height,
-			dx: x+30, //TODO: I don' know what i broke, but they are 30 pixels off...
+			dx: x,
 			dy: y,
 			dWidth: width,
 			dHeight: height,
