bda092379b9d147d1cca4fb1a6f95b8171d17d84
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-04-20 20:33:05 -0600

    towers now cost money. fixed traveling over towers bug

--- game/scripts/map.js
+++ game/scripts/map.js
@@ -286,38 +286,38 @@ var createMap = function () {
     }
 
     that.directPath = function({x,y}, goal) {
-        that = [];
+        var toReturn = [];
         while(x !== goal.x || y !== goal.y) {
             if(x > goal.x) {
                 x--;
-                that.push({
+                toReturn.push({
                     x: Math.floor(x * 1000 / rowColSize + rowColSize / 2),
                     y: Math.floor(y * 1000 / rowColSize + rowColSize / 2)
                 });
             }
             if(x < goal.x) {
                 x++;
-                that.push({
+                toReturn.push({
                     x: Math.floor(x * 1000 / rowColSize + rowColSize / 2),
                     y: Math.floor(y * 1000 / rowColSize + rowColSize / 2)
                 });
             }
             if(y > goal.y) {
                 y--;
-                that.push({
+                toReturn.push({
                     x: Math.floor(x * 1000 / rowColSize + rowColSize / 2),
                     y: Math.floor(y * 1000 / rowColSize + rowColSize / 2)
                 });
             }
             if(y < goal.y) {
                 y++;
-                that.push({
+                toReturn.push({
                     x: Math.floor(x * 1000 / rowColSize + rowColSize / 2),
                     y: Math.floor(y * 1000 / rowColSize + rowColSize / 2)
                 });
             }
         }
-        return that;
+        return toReturn;
     }
 
     return that;
--- game/scripts/utils.js
+++ game/scripts/utils.js
@@ -16,7 +16,7 @@ function startLevel() {
 function addTowerToSystem() {
 	if(vm.placeTower === '')return;
 	if(!vm.mousePosition) return;
-	if(vm.money < 100) return; // tower cost
+	if(vm.money < towers.towerCosts[vm.placeTower]) return; // tower cost
 	var position = { x: Math.floor(vm.mousePosition.x / 50),
 		y: Math.floor(vm.mousePosition.y / 50)};
 	if(!map.validPosition(position)) return;
@@ -24,7 +24,8 @@ function addTowerToSystem() {
         type: towers.TowerType[vm.placeTower],
         pos: position
     });
-    vm.placeTower = '';
+	vm.money -= towers.towerCosts[vm.placeTower];
+	vm.placeTower = '';
 }
 
 function quitGame() {
